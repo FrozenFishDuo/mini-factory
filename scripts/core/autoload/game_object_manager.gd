a27b6b91b12f26a8b09d927ee76f388c1876caf6
FrozenFishDuo <frozenfishduo@gmail.com>
2025-04-29 02:37:25 -0700

    feat: game object registery (#2)
    * feat: added game object registry
    
    * fixing branching
    
    ---------
    
    Co-authored-by: SalmonCobra <Benji.jr.thayer@gmail.com>

---
+++ scripts/core/autoload/game_object_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+static var _id_count: int = -1
+static var object_registry: Array = []
+
+## Registers a new game object and returns its unique id.
+static func register_game_object(game_object: GameObject) -> int:
+	_id_count += 1
+	var registry_item: Array = [_id_count, GameObject]
+	object_registry.append(registry_item)
+	return _id_count
+# test
