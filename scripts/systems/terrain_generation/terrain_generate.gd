9c7cf8597081093b950feb235630e723acdf49fc
ArcticColt <arcticcoltdev@gmail.com>
2025-04-29 22:31:11 -0700

    generate grass patches

--- scripts/systems/terrain_generation/terrain_generate.gd
+++ scripts/systems/terrain_generation/terrain_generate.gd
@@ -1,9 +1,9 @@
 extends Node
 
-@onready var tile_map := $"../Terrain"
+@onready var tile_map := self
 
 func _ready():
-	var grid = GenerateNoise.generate_2d(1337, 0.5, 4, 0.9)
+	var grid = GenerateNoise.generate_2d(1337, 0.5, 5, 0.05)
 	for position in grid:
 		var value = grid[position]
-		tile_map.set_cell(position, 0, Vector2i(randi_range(0, 2), roundi(value*3)))
+		tile_map.set_cell(position, 0, Vector2i(randi_range(0, 2), 2+round(value*2)))
