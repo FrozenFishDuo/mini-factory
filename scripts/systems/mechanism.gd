d329c03b937c870bd63d1674630f628c7e9e2385
SalmonCobra <Benji.jr.thayer@gmail.com>
2025-05-02 20:28:52 -0700

    redid most everything lol

---
+++ scripts/systems/mechanism.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name mechanism
+
+var id: int = 0
+
+# these arrays contain ids to figure out what its connections are
+var input: Array = []
+var output: Array = []
+
+var contents: Array = []
+
+var progress: int = 0
+
+func _ready() -> void:
+	return
+	
